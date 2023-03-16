2d6c00f7b6b0a10f234f85ff2118ea5ac4e1159c
Ossamahendi21 <oussamahindi2@gmail.com>
2023-03-16 11:46:00 -0700

    C - Hello, World with UNIX and Add Script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  *main .A cprogram taht prints a line of the standard error
+  *Return: 1 (Success)
+  */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+return (1);
+}
