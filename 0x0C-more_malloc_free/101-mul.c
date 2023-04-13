0daedc8b05e7eedade6d41a4bd462f3512bfe5a0
Ossamahendi21 <oussamahindi2@gmail.com>
2023-04-13 14:47:38 -0700

    0x0C. C - More malloc, free and more ...

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,38 +1,72 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <stdio.h>
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
-**/
+* main - multiplies two positive numbers.
+* @argc: number of arguments
+* @argv: the values in each argc element
+*
+* Description: its like infinite add but with multiplication
+* Return: print the result. return 0 if success
+*/
 
-int main(int argc, char *argv[])
+# IF EITHER NUMBER ARE ZERO RETURN 0;
+##SHOULD CHECK IF BEGINNING LEADING NUMBER IS ZERO. 
+
+
+int main(int argc, char **argv)
 {
-	unsigned long mul;
-	int i, j;
+	int size1 = 0;
+	int size2 = 0;
+	int product_size = 0;
+	int product = 0;
+	int overflow = 0;
+	int index = 0;
+	int tab = 0;
 
-	if (argc != 3)
+	while (argv[1][size1])//strlength and checks if digit
 	{
-		printf("Error\n");
-		exit(98);
+		ISDIGIT\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\##################
+		size1++;
 	}
-
-	for (i = 1; i < argc; i++)
+	while (argv[2][size2])//strlength and checks if digit
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		ISITADIGIT########################################################
+		size2++;
+	}
+	product_size = size1 + size2 + 1;//calculate the range we need
+	product = malloc(sizeof(char) * product_size);//allocates space
+	if (!product)
+		return (98);
+	while (product_size >= 0)//initialize all to 0
+		product[product_size] = '0';
+	size1--;//because length is 1 more than index
+	size2--;
+	while (size2 >= 0)
+	{
+		index = tab;
+		for (j = size1; j >= 0; j--)
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			product = (argv[1][j]-'0') * (argv[2][size2]-'0');
+			product += product[index] -'0';
+			product += overflow;
+			overflow = product / 10;
+			product[index] = product % 10 + '0';
+			index++;
 		}
+		if (overflow)
+		{
+			product[index] = overflow;
+			index++;
+		}
+		overflow = 0;
+		size2--;
+		tab++;
 	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
+	product[index] ='\0';
+	reversestring\\\\\###########################################################
+	printf("%s\n", product);
+	free(product);
 	return (0);
 }
